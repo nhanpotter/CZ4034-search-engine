625d6bdd1ad47e4761c455986d44079bfd46e5eb
Nguyen Tien Nhan <nhanpotter8a@gmail.com>
2021-03-19 11:31:14 +0800

    minor

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,7 @@ source .venv/bin/activate
 pip3 install -r requirements.txt
 python3 migrate_data.py
 ps axf|grep "flask"|grep -v grep| awk '{print "kill -9 " $1}'|sh
+export FLASK_APP=server.py
 nohup flask run --host=0.0.0.0 > nohup.out 2>&1 &
 
 echo "Done deploy"
