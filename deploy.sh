62b73f771ae2f20e2b8af42a219fac70e9d9c2ea
Nguyen Tien Nhan <nhanpotter8a@gmail.com>
2021-03-19 11:27:14 +0800

    add deploy script

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+git pull
+source .venv/bin/activate
+pip3 install -r requirements.txt
+python3 migrate_data.py
+ps axf|grep "flask"|grep -v grep| awk '{print "kill -9 " $1}'|sh
+nohup flask run --host=0.0.0.0 > nohup.out 2>&1 &
+
+echo "Done deploy"
