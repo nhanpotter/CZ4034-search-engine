4ed4c717b277dadc4c240ae335a5dca6f3626e9c
Nguyen Tien Nhan <nhanpotter8a@gmail.com>
2021-03-25 18:43:25 +0800

    add deploy script without migration

---
+++ deploy_no_migrate.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+git pull
+source .venv/bin/activate
+pip3 install -r requirements.txt
+ps axf|grep "flask"|grep -v grep| awk '{print "kill -9 " $1}'|sh
+export FLASK_APP=server.py
+nohup flask run --host=0.0.0.0 > nohup.out 2>&1 &
+
+echo "Done deploy"
